8185552dfdad84d0bbad808538e241da58f4b4ec
Ericxsansouza <erick.sousa@sptech.school>
2022-11-14 18:40:42 -0300

    Conexão com BD okay, inserindo usuário

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,7 +20,7 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    database: "webViz",
+    database: "ProjectIndividual",
     user: "root",
     password: "123456789",
 };
