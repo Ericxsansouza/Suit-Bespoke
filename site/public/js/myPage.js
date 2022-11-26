1defa36bb98e1bb5fd40f6b3f728d7a2dc1cd222
Ericxsansouza <erick.sousa@sptech.school>
2022-11-26 16:01:51 -0300

    Feito a lógica da página do cliente | FOR e VETOR

--- site/public/js/myPage.js
+++ site/public/js/myPage.js
@@ -2,3 +2,73 @@ function deslogar(){
     window.location="index.html"
 }
 
+
+function exibirEndereco(){
+    contentBlock.style.display = 'flex';
+    contentBlock.style.flexDirection = 'column'
+}
+
+const listaEndereco = [];
+function cadastrarEndereco(){
+    var endereco = buscarEnderecoDoCliente.value;
+    listaEndereco.push(endereco);
+
+    exibicaoDoEndereco.innerHTML = "";
+    for(var i = 0; i < listaEndereco.length; i++){
+        exibicaoDoEndereco.innerHTML += `
+        ${listaEndereco[i]}<br>
+        `
+    }
+    contentBlock.style.display = 'none';
+} 
+
+// Fim endereço
+
+// Começo nome
+function exibirNomeDoCliente(){
+    contentBlockDoNome.style.display = 'flex';
+    contentBlockDoNome.style.flexDirection = 'column'
+}
+
+const listaNome = [];
+function cadastrarNome(){
+    var nomeCliente = buscarNomeDoCliente.value;
+    listaNome.push(nomeCliente);
+
+    contentBlockDoNome.innerHTML = "";
+    nameUser.innerHTML = "";
+    for(var i = 0; i < listaNome.length; i++){
+        nameUser.innerHTML += `
+        ${listaNome[i]}<br>
+        `
+        resultName.innerHTML = `
+        ${listaNome[i]}
+        `
+    }
+
+    contentBlockDoNome.style.display = 'none';
+}
+
+//  Fim nome
+
+// Começo email
+function exibirEmailDoCliente(){
+    contentBlockDoEmail.style.display = 'flex';
+    contentBlockDoEmail.style.flexDirection = 'column'
+}
+
+const listaEmail = [];
+function cadastrarEmail(){
+    var emailCliente = buscarEmailDoCliente.value;
+    listaEmail.push(emailCliente);
+
+    emailUser.innerHTML = "";
+    for(var i = 0; i < listaEmail.length; i++){
+        emailUser.innerHTML += `
+        ${listaEmail[i]}<br>
+        `
+    }
+    contentBlockDoEmail.style.display = 'none';
+}
+
+// Fim email
