d11bf879d9083502e9bb7f7fbf0c754c55feb117
Ericxsansouza <erick.sousa@sptech.school>
2022-11-29 21:26:58 -0300

    Alt

--- site/public/js/script.js
+++ site/public/js/script.js
@@ -4,7 +4,7 @@ window.addEventListener("scroll", function () {
 });
 
 function abrirNav() {
-    document.getElementById("actionNav").style.width = '35%';
+    document.getElementById("actionNav").style.width = '30%';
 }
 
 function fecharNav() {
